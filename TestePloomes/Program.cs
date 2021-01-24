ea1fbb7cfd90665f5750d3f299120b04b94c8c91
Diogo <diogo.zmm@outlook.com>
2021-01-23 21:00:09 -0300

    First commit

---
+++ TestePloomes/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace TestePloomes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /************** CRIAR CLIENTE **************/
+            Console.WriteLine("---------- Criar cliente ----------\n");
+            JObject cliente = new JObject();
+
+            Console.WriteLine("Você deseja criar um novo cliente? (s/n)");
+            string decisao = Console.ReadLine();
+
+            if (decisao == "s")
+            {               
+                Console.Write("\nDigite o nome do cliente: ");
+                string nomeCliente = Console.ReadLine();
+                cliente.Add("Name", nomeCliente);
+
+                Console.WriteLine("\nO cliente é pessoa física ou jurídica? (f/j)");
+                string tipoPessoa = Console.ReadLine();
+
+                if (tipoPessoa == "f")
+                    cliente.Add("TypeId", "2");
+                else if (tipoPessoa == "j")
+                    cliente.Add("TypeId", "1");
+
+                AdicionarCliente(cliente);
+            }
+            else if (decisao == "n")
+            {
+                Console.WriteLine("Próxima etapa...\n");
+            }
+
+            /************** VERIFICAR ÚLTIMO CLIENTE CADASTRADO **************/
+            Console.WriteLine("---------- Último cliente cadastrado ----------\n");
+            JArray clientesCadastrados = RequestHandler.MakePloomesRequest($"Contacts", RestSharp.Method.GET);            
+            string ultimoNomeCadastrado = clientesCadastrados.Last["Name"].ToString(); 
+            Console.WriteLine($"O último cliente cadastrado é: {ultimoNomeCadastrado.ToUpper()}\n");
+
+            /************** CRIAR NOVA NEGOCIAÇÃO **************/
+            Console.WriteLine("---------- Criar Negociação ----------\n");
+
+            JObject negocio = new JObject();
+
+            Console.WriteLine("Digite o nome do negócio");
+            string nomeNegocio = Console.ReadLine();
+            negocio.Add("Title", nomeNegocio);
+            Console.WriteLine("\nO negócio será vinculado ao último cliente cadastrado: " + ultimoNomeCadastrado.ToUpper() + "\n");
+            negocio.Add("ContactId", clientesCadastrados.Last["Id"].ToString());
+
+            AdicionarNegociacao(negocio);
+
+            /************** VERIFICAR ÚLTIMO NEGÓCIO CADASTRADO **************/
+            Console.WriteLine("---------- Último negócio cadastrado ----------\n");
+            JArray negociosCadastrados = RequestHandler.MakePloomesRequest($"Deals", RestSharp.Method.GET);
+            string ultimoNegocio = negociosCadastrados.Last["Title"].ToString();
+            Console.WriteLine("Negócio cadastrado: " + ultimoNegocio.ToUpper() + "\n");
+
+            /************** CRIAR NOVA TAREFA **************/
+            Console.WriteLine("---------- Criar tarefa ----------\n");
+
+            JObject task = new JObject();
+
+            Console.WriteLine("Digite o nome da tarefa. Esta tarefa será vinculada ao último negócio cadastrado: " + ultimoNegocio.ToUpper() + "\n");
+            string nomeTarefa = Console.ReadLine();
+            task.Add("Title", nomeTarefa);
+            task.Add("ContactId", clientesCadastrados.Last["Id"].ToString());
+            task.Add("DealId", negociosCadastrados.Last["Id"].ToString());
+
+            AdicionarTarefa(task);
+
+            /************** VERIFICAR ÚLTIMA TAREFA CADASTRADA **************/
+            Console.WriteLine("---------- Última tarefa cadastrada ----------\n");
+            JArray tarefasCadastradas = RequestHandler.MakePloomesRequest($"Tasks", RestSharp.Method.GET);
+            string ultimaTarefa = tarefasCadastradas.Last["Title"].ToString();
+            Console.WriteLine("Tarefa cadastrada: " + ultimaTarefa.ToUpper() + "\n");
+
+            /************** ALTERAR VALOR DO NEGÓCIO **************/
+            Console.WriteLine("---------- Alterar Valor ----------\n");
+            Console.WriteLine("Valor sendo alterado...\n");
+
+            AtualizarValor(negociosCadastrados.Last["Id"].ToString());
+            Console.WriteLine("Valor atualizado!\n");
+
+            /************** FINALIZAR TAREFA ABERTA **************/
+            Console.WriteLine("---------- Finalizar Tarefa ----------\n");
+            Console.WriteLine("Tarefa sendo finalizada...\n");
+
+            FinalizarTarefa(tarefasCadastradas.Last["Id"].ToString());
+
+            /************** GANHAR NEGOCIAÇÃO **************/
+            Console.WriteLine("---------- Ganhar Negociação ----------\n");
+            Console.WriteLine("Negociação sendo ganha...\n");
+
+            GanharNegociacao(negociosCadastrados.Last["Id"].ToString());
+
+            /************** ESCREVER NO HISTÓRICO DO CLIENTE **************/
+            Console.WriteLine("---------- Escrever no histórico ----------\n");
+            Console.WriteLine("Histórico sendo escrito...\n");
+
+            JObject interacao = new JObject();
+            interacao.Add("ContactId", clientesCadastrados.Last["Id"].ToString());
+            interacao.Add("Content", "Negócio Fechado!");
+
+            AdicionarInteracao(interacao);
+            Console.WriteLine("Finalizado!");
+        }
+
+        // MÉTODOS CRIADOS PARA MELHORAR A LEITURA DO CÓDIGO. ASSIM, FACILITA-SE O ENTENDIMENTO EM RELAÇÃO AOS MÉTODOS POST SENDO ADICIONADOS DURANTE A EXECUÇÃO.
+        public static void AdicionarCliente(JObject cliente)
+        {
+            RequestHandler.MakePloomesRequest($"Contacts", RestSharp.Method.POST, cliente);
+        }
+       
+        public static void AdicionarNegociacao(JObject negocio)
+        {
+            RequestHandler.MakePloomesRequest($"Deals", RestSharp.Method.POST, negocio);
+        }
+
+        public static void AdicionarTarefa(JObject tarefa)
+        {
+            RequestHandler.MakePloomesRequest($"Tasks", RestSharp.Method.POST, tarefa);
+        }
+
+        public static void AdicionarInteracao(JObject interacao)
+        {
+            RequestHandler.MakePloomesRequest($"InteractionRecords", RestSharp.Method.POST, interacao);
+        }
+
+        public static void FinalizarTarefa(string taskId)
+        {
+            JObject finish = new JObject
+            {
+                {"Finished", "true" }
+            };
+            RequestHandler.MakePloomesRequest($"Tasks({taskId})/Finish", RestSharp.Method.POST, finish);
+        }
+
+        public static void AtualizarValor(string dealId)
+        {
+            JObject update = new JObject
+            {
+                {"Amount", "15000.00" }
+            };
+            RequestHandler.MakePloomesRequest($"Deals({dealId})", RestSharp.Method.PATCH, update);
+        }
+
+        public static void GanharNegociacao(string dealId)
+        {
+            JObject win = new JObject
+            {
+                {"StatusId", "2" }
+            };
+            RequestHandler.MakePloomesRequest($"Deals({dealId})/Win", RestSharp.Method.POST, win);
+        }
+    }
+}
---
+++ TestePloomes/RequestHandler.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestePloomes
+{
+    class RequestHandler
+    {
+        private static string PLOOMES_API_PATH = "https://api2.ploomes.com/";
+        private static HttpClient ploomesClient;
+        private static string uk = "BA104C4B382F621B7F293F9FF3E49A168B68FFA7BA34B6860A157EB6C79EE6D06A942AC43B040C3AFE39796594005FC299A25A4477E3B5E3A770848066AE9678";
+
+        public static void instantiatePloomesConnection()
+        {
+            ploomesClient = new HttpClient();
+            ploomesClient.DefaultRequestHeaders.Add("User-Key", uk);
+            ploomesClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public static JArray MakePloomesRequest(string url, Method method, JObject json = null)
+        {
+            try
+            {
+                instantiatePloomesConnection();
+
+                System.Threading.Thread.Sleep(1000);
+                string response = string.Empty;
+                url = PLOOMES_API_PATH + url;
+
+                if (method == Method.GET)
+                    response = ploomesClient.GetAsync(url).Result.Content.ReadAsStringAsync().Result;
+                else if (method == Method.POST)
+                {
+                    if (json != null)
+                        response = ploomesClient.PostAsync(url, new StringContent(json.ToString())).Result.Content.ReadAsStringAsync().Result;
+                    else
+                        response = ploomesClient.PostAsync(url, new StringContent(new JObject().ToString())).Result.Content.ReadAsStringAsync().Result;
+                }
+                else if (method == Method.DELETE)
+                {
+                    ploomesClient.DeleteAsync(url);
+                    return null;
+                }
+                else if (method == Method.PATCH)
+                {
+                    var content = new ObjectContent<JObject>(json, new JsonMediaTypeFormatter());
+                    var request = new HttpRequestMessage(new HttpMethod("PATCH"), url) { Content = content };
+                    response = ploomesClient.SendAsync(request).Result.Content.ReadAsStringAsync().Result;
+
+                    Console.WriteLine(response.ToString());
+                }
+
+                return JsonConvert.DeserializeObject<JObject>(response)["value"] as JArray;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error in MakePloomesRequest method --- " + e.Message);
+                throw e;
+            }
+        }
+    }
+}
